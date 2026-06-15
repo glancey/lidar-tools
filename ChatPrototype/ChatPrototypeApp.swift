d97f128500d2e3e029c1a9dce19e26c199984c74
Glenn Silverman <silvergl@icloud.com>
2026-06-15 12:15:13 -0700

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Glenn Silverman on 6/15/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatPrototypeTests/ChatPrototypeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeTests.swift
+//  ChatPrototypeTests
+//
+//  Created by Glenn Silverman on 6/15/26.
+//
+
+import Testing
+@testable import ChatPrototype
+
+struct ChatPrototypeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
